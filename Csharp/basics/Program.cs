607308dfc450b69e8695d13e954defafefc43d46
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-08-05 11:04:35 +0100

    some basics

---
+++ Csharp/basics/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace Csharp
+{
+    public class Bsics
+    {
+        static void Main()
+        {
+            /* *********   Parse  ********* */
+
+
+            /*
+
+            Console.WriteLine("Enter your birth year : ");
+
+            string year = Console.ReadLine();
+
+            int str_to_int = int.Parse(year);
+
+            Console.Write($"Your age is : {2025 - str_to_int}");
+
+
+
+            */
+
+
+
+            /*  ***********  Arrays    ****************  */
+
+
+            /*
+
+            int[] Schools = {13, 37, 42, 19};
+
+            Array.Sort(Schools);   // - sort
+
+            int[] copy_array = new int[4];  // - copy
+
+            Array.Copy(Schools, copy_array, Schools.Length);
+
+            Console.WriteLine(copy_array[1]);
+
+
+            for (int i = 0; i < Schools.Length; i++)
+                Console.WriteLine($" index {i}  : value : {Schools[i]}");
+
+
+
+            */
+
+
+
+
+        }
+    }
+}
+
