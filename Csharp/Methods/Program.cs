b66469d407e940968ee0178988fb58c1593000bb
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-08-05 12:48:01 +0100

    Methods : syntax

---
+++ Csharp/Methods/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+
+
+        /*  ************  Methods   **************  */
+
+
+        // syntax of Method
+
+
+
+        // AccessModifier   modifer   return type   MethodeName(parameter1,  parametere2) {}
+
+
+        // AccessModifier :  public    private   protected    internal -> Accessible within the same project (assembly)  
+
+        // modifier       :  static   virtual   abstract  new   partial 
+
+        // return  type   :   int      string   ...
+
+
+
+        public static int Multiply(int x, int y)
+        {
+
+            /*
+                public → accessible from anywhere
+
+                static → can be called without creating an object
+
+                int → returns an integer
+
+                Multiply → method name
+
+                (int x, int y) → two input parameters
+
+                return x * y; → result is returned
+
+
+                 
+             
+             
+             */
+
+
+            return x * y;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("the result of 5 x 5 = {0}", Multiply(5, 5));
+
+        }
+    }
+}
