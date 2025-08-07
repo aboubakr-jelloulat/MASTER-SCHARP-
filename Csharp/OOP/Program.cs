8383cdf9b4791530a4536b3b851c4b295ab12e66
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-08-07 10:37:03 +0100

    Variables Scope

---
+++ Csharp/OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using  OOP.Variable_Scopes;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //VariableScopes.VariablesScopes();
+
+            VariableScopes.BlockScope();
+
+        }
+    }
+}
---
+++ Csharp/OOP/Variable Scopes/VariableScopes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Variable_Scopes
+{
+    public class VariableScopes
+    {
+
+        // ***** [ Variables Scopes ]
+
+        private string _name = "Hej ";
+
+        public static void VariablesScopes()
+        {
+            //Console.WriteLine(_name); // you can't acces it non-static 
+
+            var MyScope = new VariableScopes();
+
+            Console.WriteLine(MyScope._name);
+
+        }
+
+        public static void BlockScope()
+        {
+            int x = 1337;
+
+            while (true)
+            {
+                int y = 42;
+
+                Console.WriteLine($"im y visible here : {y}");
+
+                break;
+            }
+
+            //Console.WriteLine($"im y not visible here : {y}");
+
+        }
+
+       
+        
+    }
+}
