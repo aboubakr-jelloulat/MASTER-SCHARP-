0ea2c5091289c576f2400f9561e85a4228ecde23
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-08-13 11:22:36 +0100

    Sealed Class

---
+++ Csharp/OOP/Inheritance/05_Sealed Classes & Sealed Method/Sealed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Inheritance._05_Sealed_Classes___Sealed_Method
+{
+    internal class Sealed
+    {
+    }
+}
