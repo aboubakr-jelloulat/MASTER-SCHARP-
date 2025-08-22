6e80d97f7d1509b0cfbdec04c54990d091283f62
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-08-22 15:02:25 +0100

    Task - Based Asynchronous

---
+++ Csharp/Advanced Csharp/07_Enumerables/Enumerables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Csharp._07_Enumerables
+{
+    internal class Enumerables
+    {
+    }
+}
